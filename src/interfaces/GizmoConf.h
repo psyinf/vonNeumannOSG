66e3a11c0fa6abdbc769c11e5e5b957f89bb5929
psy_inf <sebastian@smesserschmidt.de>
2022-09-14 00:38:16 +0200

    json gizmo configuration with default

--- src/interfaces/GizmoConf.h
+++ src/interfaces/GizmoConf.h
@@ -7,11 +7,11 @@ namespace nsConfig
 {
 struct Gizmo
 {
-    bool                 enabled;
+    bool                 enabled = true;
     std::string          type;    // type of the gizmo (e.g. what is the data source)
     std::string          mapping; // how is the gizmo modified by the value (scale, direction ..)
     std::string          model;
     PositionAttitudeConf pat;
 };
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Gizmo, enabled, type, mapping, model, pat)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(Gizmo, enabled, type, mapping, model, pat)
 } // namespace nsConfig
